6e0f11e1d522f08dcce3ccd7fd10bc5ea714c258
Nikward <114798709+Nikward@users.noreply.github.com>
2022-12-02 09:44:11 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,4 +1,39 @@
-CREATE TABLE IF NOT EXISTS singers (
-	singer_id SERIAL PRIMARY KEY,
-	name ARCHAR(60) NOT NULL
+CREATE TABLE IF NOT EXISTS Singers (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Genres (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS SingersGenres(
+	singer_id INTEGER REFERENCES Singers(id),
+	genre_id INTEGER REFERENCES Genres(id),
+	CONSTRAINT pk_sg PRIMARY KEY (singer_id, genre_id)
+);
+CREATE TABLE IF NOT EXISTS Albums(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	year INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS SingersAlmums(
+	Singer_id INTEGER REFERENCES Singers(id),
+	Albums_id INTEGER REFERENCES Albums(id),
+	CONSTRAINT pk_sa PRIMARY KEY (Singer_id, Albums_id)
+);
+CREATE TABLE IF NOT EXISTS Tracks(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	duration VARCHAR(80),
+	albom_id INTEGER REFERENCES Albums(id)
+);
+CREATE TABLE IF NOT EXISTS Collections(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	year INTEGER
+);
+CREATE TABLE IF NOT EXISTS CollectionTracks(
+	collection_id INTEGER REFERENCES Collections(id),
+	track_id INTEGER REFERENCES Tracks(id),
+	CONSTRAINT pk_ct PRIMARY KEY (collection_id, track_id)
 );
