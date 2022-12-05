77a2d5c657b55cd9ea73b7926db3b312d96f19eb
Nikward <114798709+Nikward@users.noreply.github.com>
2022-12-05 09:02:50 +0300

    Update employee BD CREATE.sql

--- employee BD CREATE.sql
+++ employee BD CREATE.sql
@@ -1,12 +1,12 @@
 
 CREATE TABLE department(
-	department_id SERIAL PRIMARY KEY,
-	name varchar(80) UNIQUE NOT null
+department_id SERIAL PRIMARY KEY,
+name varchar(80) UNIQUE NOT null
 );
 	
 CREATE TABLE employee(
-	employee_id SERIAL PRIMARY KEY,
-	name VARCHAR(60) NOT NULL,
-	senior_id INTEGER NOT NULL REFERENCES employee(employee_id),
-	department_id INTEGER NOT NULL REFERENCES department(department_id)
+employee_id SERIAL PRIMARY KEY,
+name VARCHAR(60) NOT NULL,
+senior_id INTEGER NOT NULL REFERENCES employee(employee_id),
+department_id INTEGER NOT NULL REFERENCES department(department_id)
 );
